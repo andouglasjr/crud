365cf7fd652c5b72c6aae8ba1a44dc9206b20e3d
andouglasjr <andouglas.silva@escolar.ifrn.edu.br>
2023-06-27 14:13:39 -0300

    Test git_copy

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import page.select as select
 
 #criando a barra lateral do menu
 st.sidebar.title('Menu')
-selectbox = st.sidebar.selectbox('Ação', ['Inserir', 'Consultar'])
+selectbox = st.sidebar.selectbox('Ação', ['Inserir', 'Consultar','Atualizar'])
 
 if selectbox == 'Inserir':
     insert.inserir()
